e2c9f6c1070a982c05d83f8b38c6d97a9e58d76b
Sean Andrew Stanek <146111111+Sean-Andrew-Stanek@users.noreply.github.com>
2023-12-12 23:54:41 +0900

    first endpoint

--- server.js
+++ server.js
@@ -1,7 +1,20 @@
+//Modules
 const express = require('express');
 const path = require('path');
+const MovieModel = require('.models/movie.model');
+const DirectorModel = require('.models/director.model');
+const GenreModel = require('.model/genre.model');
 
+//Express Setup
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({extended: true }));
+
+//Collections
+const Movies = MovieModel.Movie;
+const Directors = DirectorModel.Director;
+const Users = GenreModel.User;
+
 
 //CONSTANTS
 let serverPort = 8080;
@@ -19,3 +32,25 @@ app.get('/', (req, res) => {
 app.listen(serverPort, () => {
     console.log(`Server started on port ${serverPort}`);
 });
+
+////////////
+//
+//  Movie
+//
+////////////
+
+
+//Get all movies
+app.get('/movies', async(req, res) => {
+    await Movies.find()
+        .then((movies) => {
+            res.status(201).json(movies);
+        })
+        .catch((error) => {
+            console.error(error);
+            res.status(500).send(`Error: ${error}`);
+        });
+});
+
+//Add movie
+//app.post('/movies',)
